a3d4a4ee1f42fd17d40087e3e28f9c2394c5574b
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-07 03:15:22 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ attr_accessor :name, :price
       item_name = req.path.split("/items/").last
       if @@items.include?(item_name)
         item = @@items.detect{|i| i.name == item_name}
-        return item.price
+        item.price
       else
         resp.write "Item not found"
         resp.status = 400
