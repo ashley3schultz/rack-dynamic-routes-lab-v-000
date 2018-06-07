ef1e872449e6dcd049e28a262a7dfa91877ed328
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-07 02:53:04 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,14 +1,14 @@
-class Application 
+class Application
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path=="/items"
-      resp.write 
-    else 
+      resp.write
+    else
       resp.write "page Not Found"
       resp.status = 404
-    end 
-  end 
+    end
+  end
 end 
