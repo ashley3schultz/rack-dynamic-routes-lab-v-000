bb4d97910cdc0b47a18d2f41e360cca0d3ee4462
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-07 03:06:15 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ attr_accessor :name, :price
       item_name = req.path.split("/items/").last
       item_id = @@items.find{|i| i.name == item_name}
 
-      resp.write item_id.price
+      resp.write item_id.price || nil
     else
       resp.write "Route not found"
       resp.status = 404
