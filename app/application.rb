93ac28fc1e10536c4f6dbd7152d3d67a9b560086
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-07 03:02:35 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,11 +7,11 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
- 
+
       item_name = req.path.split("/items/").last #turn /songs/Sorry into Sorry
       item = @@items.find{|i| i.name == item_name}
 
-      resp.write item.price        
+      resp.write item.price
     else
       resp.write "page Not Found"
       resp.status = 404
