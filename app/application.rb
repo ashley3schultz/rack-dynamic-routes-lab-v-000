54860ef103dc004762e12b32178ac6020ef7479c
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-07 03:11:33 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,18 +9,14 @@ attr_accessor :name, :price
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       if @@items.include?(item_name)
-        item_id = @@items.find{|i| i.name == item_name}
-        resp.write item_id.price
+        item = @@items.find{|i| i.name == item_name}
+        resp.write item.price
       else
         resp.status = 400
       end
     else
       resp.write "Route not found"
       resp.status = 404
-    end
-
-    def price
-      @price
     end
     resp.finish
   end
