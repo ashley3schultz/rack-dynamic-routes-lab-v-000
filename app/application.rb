85143796c598972013109b1dc12d2b2edbaff875
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-07 02:52:50 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,14 @@
+class Application 
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path=="/items"
+      resp.write 
+    else 
+      resp.write "page Not Found"
+      resp.status = 404
+    end 
+  end 
+end 
