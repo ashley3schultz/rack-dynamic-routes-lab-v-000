1ef09ff3a277d9c785246adb13f2eba0a352fad1
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-07 03:30:25 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      if @@items.all.include?(item_name)
+      if @@items.include?(item_name)
         i = @@items.detect { |i| i.name == item_name}
         resp.write i.price
         resp.status = 200
