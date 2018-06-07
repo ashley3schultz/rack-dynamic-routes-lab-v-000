e75e4f043989da710101b80a7497d79cd128bf02
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-07 03:02:27 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,21 +1,17 @@
 class Application
 
+  @@items = []
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
  
-      song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
-      song = @@songs.find{|s| s.title == song_title}
- 
-      resp.write song.artist
-    end
+      item_name = req.path.split("/items/").last #turn /songs/Sorry into Sorry
+      item = @@items.find{|i| i.name == item_name}
 
-
-
-
-        
+      resp.write item.price        
     else
       resp.write "page Not Found"
       resp.status = 404
